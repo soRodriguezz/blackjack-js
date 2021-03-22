5d4718944913c9d5ce0f2279afe3be1210892d84
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-03-22 18:55:59 -0300

    validaciones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,7 +6,11 @@ let puntosJugador = 0,
 
 // Referencias de HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+
 const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 //ANCHOR: Creacion deck
@@ -49,6 +53,7 @@ const pedirCarta = () => {
 const valorCarta = ( carta ) => {
     
     //NOTE: corto el string desde la posicioin 0 a el penultimo.
+    //NOTE: El substring() método devuelve un subconjunto de un objeto String.
     const valor = carta.substring(0, carta.length - 1);
     
     return (isNaN( valor )) ? 
@@ -67,6 +72,41 @@ const valorCarta = ( carta ) => {
 }
 // const valor = valorCarta( pedirCarta() );
 // console.log({ valor });
+
+// Turno computadora
+const turnoComputadora = ( puntosMinimos ) => {
+
+    do {
+
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML[1].innerText = puntosComputadora;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append( imgCarta );
+
+        if ( puntosMinimos > 21 ) break;
+
+    } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos < 21 ) );
+
+    setTimeout( () => {
+        if ( puntosComputadora === puntosMinimos ) {
+            alert('¡Nadie gana!');
+        } else if ( puntosMinimos > 21 ) {
+            alert('¡La computadora gana!');
+        } else if ( puntosComputadora > 21 ){
+            alert('¡Ganaste!');
+        } else if (puntosMinimos === 21) {
+            alert('¡Ganaste!');
+        }else {
+            alert('¡La computadora gana!');
+        }
+    }, 10);
+
+}
 
 // Eventos
 //NOTE: funcion que se manda como argumento es conocido como callback
@@ -87,8 +127,39 @@ btnPedir.addEventListener('click', () => {
     if ( puntosJugador > 21) {
         console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
     }else if ( puntosJugador === 21 ) {
         console.warn('21, genial');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
     }
+});
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora( puntosJugador );
+});
+
+btnNuevo.addEventListener('click', () => {
+    
+    console.clear();
+    deck = [];
+    deck = crearDeck();
+    
+    
+    puntosComputadora = 0;
+    puntosJugador = 0;
+
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+
+    divCartasComputadora.innerHTML = ''
+    divCartasJugador.innerHTML = '';
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+
 });
